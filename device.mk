f876196ade84c9181baceff276a5fc754b80916b
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 23:25:18 -0700

    begonia: Enable vendor and system property isolation
    Change-Id: Ic73cd73c13220b804ec1c0a5fd1bd68470cf9db1

--- device.mk
+++ device.mk
@@ -200,6 +200,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# Properties
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
