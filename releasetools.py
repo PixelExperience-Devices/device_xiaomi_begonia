8115cb2fd5e49e1a9ea43c655e88998823b8dff8
XiNGRZ <chenxingyu92@gmail.com>
2019-04-29 03:23:26 +0800

    begonia: releasetools: Ship and update vbmeta image
    Change-Id: I8e9a49741e27eee01a784cd10f96c046202b7c8c

--- releasetools.py
+++ releasetools.py
@@ -33,5 +33,6 @@ def AddImage(info, basename, dest):
 
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
+  AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
   AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
   return
