AgentFabulous/twrp_xiaomi_begonia

begonia: Initial TWRP tree
Change-Id: I4b70ec408dab038ff4e6f33266aef4acb2cf22bb
Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

AndroidProducts.mk
@@ -1,0 +1,18 @@
+#
+# Copyright (C) 2019 Potato Open Sauce Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_begonia.mk

PixelExperience-Devices/device_xiaomi_begonia

Change-Id: I220c0ff41c17b7e0e05e351cd454b72bc3e5d439

AndroidProducts.mk
@@ -1,0 +1,18 @@
+#
+# Copyright (C) 2019 Potato Open Sauce Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/potato_begonia.mk