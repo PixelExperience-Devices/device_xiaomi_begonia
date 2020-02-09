e89ade2c96749a0838e5ce3ee28d629cc8e3966d
Kshitij Gupta <kshitijgm@gmail.com>
2020-02-09 17:25:48 +0000

    begonia: Add custom dtbo mk rules and append certs
    - begonia's LK appears to be checking for certs added to dtbo during
      signing. We unfortunately do not have the private keys needed to
      generate an image that the bootloader would accept.
    - dd out the 2 der certs and append them appropriately with some padding
      to make the bootloader happy. (Extracted from MIUI 10 dtbo.img)
    
    Change-Id: I26402236bcea3516fb98f5d48857106ae30b54c1
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

---
+++ dtbo/dtbo.mk
@@ -0,0 +1,24 @@
+BOARD_PREBUILT_DTBOIMAGE := $(PRODUCT_OUT)/dtbo-pre.img
+BOARD_KERNEL_DTBO_CFG := dtboimg.cfg
+MKDTBOIMG := system/libufdt/utils/src/mkdtboimg.py
+APPEND_CERTS := $(DEVICE_PATH)/dtbo/append_certs.py
+
+# BUG: mkdtboimg.py doesn't support absolute paths yet. Fix this later.
+define build-dtboimage-target-from-cfg
+    $(call pretty,"Target dtbo image from cfg: $(BOARD_PREBUILT_DTBOIMAGE)")
+    $(hide) $(MKDTBOIMG) cfg_create $@ $(KERNEL_OUT)/$(BOARD_KERNEL_DTBO_CFG) -d /
+    $(hide) chmod a+r $@
+endef
+
+define append-dtboimage-certs
+    $(call pretty,"Target signed dtbo image: $(BOARD_PREBUILT_DTBOIMAGE)")
+    $(hide) mv $(BOARD_PREBUILT_DTBOIMAGE) $(BOARD_PREBUILT_DTBOIMAGE).tmp
+    $(hide) $(APPEND_CERTS) --image $(BOARD_PREBUILT_DTBOIMAGE).tmp --cert1 \
+        $(DEVICE_PATH)/dtbo/cert1.der --cert2 $(DEVICE_PATH)/dtbo/cert2.der --output $(BOARD_PREBUILT_DTBOIMAGE)
+    $(hide) chmod a+r $@
+endef
+
+
+$(BOARD_PREBUILT_DTBOIMAGE): $(MKDTBOIMG) $(INSTALLED_KERNEL_TARGET)
+	$(build-dtboimage-target-from-cfg)
+	$(append-dtboimage-certs)
