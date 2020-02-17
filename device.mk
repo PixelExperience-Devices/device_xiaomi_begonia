0d96bdc9d7eb062a04a1f6b81a22a15e5a37d573
Kshitij Gupta <kshitijgm@gmail.com>
2020-02-17 08:03:40 +0000

    begonia: Also copy idc and kl to system image
    Change-Id: I637ea3fcef86a110b3900f37c36b61fb0edb6a08
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- device.mk
+++ device.mk
@@ -24,6 +24,15 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc \
+    $(DEVICE_PATH)/idc/uinput-goodix.idc:system/usr/idc/uinput-goodix.idc
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
+    $(DEVICE_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
