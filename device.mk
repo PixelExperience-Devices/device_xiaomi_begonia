cf52d70200edff246479d54d795fb3e3fb71483a
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    begonia: Build and enable fastbootd mode
    Change-Id: I7f4246f3f583dcae7b3bfc0d3ae1cf4d577632cb

--- device.mk
+++ device.mk
@@ -175,6 +175,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
