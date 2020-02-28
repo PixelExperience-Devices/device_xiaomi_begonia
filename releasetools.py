61e60c34874bea2046b9ae453e60043f8e0aa035
Kshitij Gupta <kshitijgm@gmail.com>
2020-02-28 09:37:44 +0000

    begonia: releasetools: Ensure userdata is mounted last
    - Needed for TEE and keymaster
    - Also remove superfluous returns
    
    Change-Id: Ibe1e49512a10fc0f76f5dd0b37a53dfe3fcea91f

--- releasetools.py
+++ releasetools.py
@@ -19,11 +19,9 @@ import re
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
-  return
 
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
-  return
 
 def AddImage(info, basename, dest):
   name = basename
@@ -32,7 +30,14 @@ def AddImage(info, basename, dest):
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
+  PatchVendor(info)
   info.script.Print("Patching firmware images...")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
   AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
-  return
+
+def PatchVendor(info):
+  info.script.Print("Patching vendor init scripts...")
+  info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/bootdevice/by-name/vendor", "/vendor");')
+  info.script.AppendExtra('run_program("/sbin/sed", "-i", "s/wait,check,formattable,quota,resize/latemount,wait,check,formattable,quota/", "/vendor/etc/fstab.mt6785");')
+  info.script.AppendExtra('run_program("/sbin/sed", "-i", "s/fstab.mt6785$/fstab.mt6785 --early\\n    mount_all \/vendor\/etc\/fstab.mt6785 --late/", "/vendor/etc/init/hw/init.mt6785.rc");')
+  info.script.AppendExtra('unmount("/vendor");')
