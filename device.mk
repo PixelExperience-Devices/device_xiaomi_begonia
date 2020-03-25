b476e34ec2c972ab2290d62e72267488cbb382ac
Aayush Gupta <aayushgupta219@gmail.com>
2020-03-25 23:27:37 +0530

    begonia: Cleanup overlays and libs
    * Drop overlays with default values as fw/base and vendor
    * Drop libs which are not required
    
    Change-Id: I52d0b5366b5b25ec0cc2800f8333fac1d978bc11
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- device.mk
+++ device.mk
@@ -38,8 +38,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    libvulkan \
-    vendor.display.config@1.0
+    libvulkan
 
 # DRM
 PRODUCT_PACKAGES += \
