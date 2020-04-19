001bab1a5ded8e3d9680c7be7854c26005a787e3
Henrique Silva <jhenrique09.mcz@hotmail.com>
2020-04-20 01:15:37 +0600

    begonia: parts: Migrate Strings To devicesettings-custom
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I2641d617a84434961d61bb67ea4fbbf76edbd6b4

--- parts/Android.mk
+++ parts/Android.mk
@@ -25,7 +25,8 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    $(TOP)/packages/resources/devicesettings/res
+    $(TOP)/packages/resources/devicesettings/res \
+    $(TOP)/packages/resources/devicesettings-custom/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
