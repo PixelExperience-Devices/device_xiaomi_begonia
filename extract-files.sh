14296f1d9afbcd0680c5a17729ae87ab7c931579
Kshitij Gupta <kshitijgm@gmail.com>
2020-05-24 17:48:31 +0530

    begonia: extract_files: Fixup kmodule paths for *.rc
    - A few init scripts included as blobs may load modules in different
      modes of operation. Give them the correct path, should they be loading
      the modules.
    
    Change-Id: I050cc349dab8f9679caab9bd26fd65d88906765d

--- extract-files.sh
+++ extract-files.sh
@@ -54,6 +54,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+    *.rc)
+        sed -i "s/vendor\/lib\/modules\//vendor\/lib\/modules_prebuilt\//g" ${2}
+        ;;
+    esac
+}
+
 # Initialize the helper for common device
 setup_vendor "${DEVICE}" "${VENDOR}" "${POTATO_ROOT}" true "${CLEAN_VENDOR}"
 
