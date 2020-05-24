3953b06fa3dc5d728e7b55aa0767507de8faf64e
Kshitij Gupta <kshitijgm@gmail.com>
2020-05-24 17:36:44 +0530

    begonia: Create symlinks for gatekeeper & vulkan
    Change-Id: I943f3d1fd00240305cad8f535f91845c239acb17
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- Android.mk
+++ Android.mk
@@ -9,4 +9,23 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),begonia)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+include $(CLEAR_VARS)
+
+VENDOR_SYMLINKS := \
+    $(TARGET_OUT_VENDOR)/lib/hw \
+    $(TARGET_OUT_VENDOR)/lib64/hw
+
+$(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Making vendor symlinks"
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+	@ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6785.so
+	@ln -sf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6785.so
+	$(hide) touch $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINKS)
+
 endif
