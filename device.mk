79b071461143949385f8225c8ea62f96308b2bc6
Zinadin Zidan <tuhinsultana075@gmail.com>
2020-06-06 05:48:10 +0600

    begonia: Add Some Configs To Overlay-pe
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I2ecf653f534feeeb2dea9123f133d720acb51812

--- device.mk
+++ device.mk
@@ -97,7 +97,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pe
 
 # Overlay from mtk-telephony-ext
 include vendor/mediatek/hardware/telephony-ext/overlay.mk
