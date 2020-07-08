00ca30c08857c2a2f4d6ca94993a827c65a60137
Kshitij Gupta <kshitijgm@gmail.com>
2020-07-08 18:56:04 +0530

    begonia: sf: Declare wide color and HDR support
    Change-Id: I1e3aefac1582e20c34122940a687233c797cf4dd

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -430,6 +430,8 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
