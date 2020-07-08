438321ec41c713dad03daeece16a75a540a99b67
Kshitij Gupta <kshitijgm@gmail.com>
2020-07-08 18:58:54 +0530

    begonia: sf: Enable running_without_sync_framework
    - Fixes tint flickers in night-light
    
    Change-Id: Ic349e855c230ceb15303379a4a6a36d0b4a6de72

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -433,5 +433,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.running_without_sync_framework=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
