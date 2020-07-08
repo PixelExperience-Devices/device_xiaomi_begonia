8e90fd089a8a7064caa8d7805d84b1f771d2d94c
Kshitij Gupta <kshitijgm@gmail.com>
2020-07-08 18:53:33 +0530

    begonia: sf: Define max buffers and vsync phase offsets
    - For whatever reason, begonia's configstore does not work and makes
      surfaceflinger use defaults
    
    Change-Id: I8ea718d03d184aa48dc764df44fe7a50afc83409

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -427,3 +427,9 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
 		persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
+
+# SurfaceFlinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
