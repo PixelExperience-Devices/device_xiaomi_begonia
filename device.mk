b6417a003e074878ae4f4a2b7f053ea31a24bc86
Kshitij Gupta <kshitijgm@gmail.com>
2020-08-01 11:38:10 +0000

    begonia: Import required system|vendor properties
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I6e3b884c7f4c2a15e27162338629e5ac35e81c37

--- device.mk
+++ device.mk
@@ -16,9 +16,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
-# Properties
--include $(DEVICE_PATH)/system_prop.mk
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
