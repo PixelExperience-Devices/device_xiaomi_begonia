abb7fa3ba27ba2702423541f7a262446ff7c8ef0
Aayush Gupta <aayushgupta219@gmail.com>
2020-08-02 13:50:31 +0000

    begonia: Drop non-required configstore@1.1.policy
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I8aa21949a17e6527b19984371bc438324e758ae3

--- device.mk
+++ device.mk
@@ -237,7 +237,6 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
     $(DEVICE_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(DEVICE_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
     $(DEVICE_PATH)/seccomp/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
