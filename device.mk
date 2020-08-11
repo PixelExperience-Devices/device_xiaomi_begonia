852689778c4e88702cae542718e2cd96b4e325d3
Aayush Gupta <aayushgupta219@gmail.com>
2020-08-11 17:25:48 +0000

    begonia: Generate privapp-permissions for hotword & mediatek apps
    Reference:[0]: https://source.android.com/devices/tech/config/perms-whitelist
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Iaca3bacc521afac89549af3148d2efcb963aa432

--- device.mk
+++ device.mk
@@ -29,6 +29,11 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0_system
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Pre-opt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
