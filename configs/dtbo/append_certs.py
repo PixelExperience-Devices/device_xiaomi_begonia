e09c2a4fac0d327def1312ac4e7c5d488f7fb549
Aayush Gupta <aayushgupta219@gmail.com>
2020-08-25 14:55:28 +0000

    begonia: Rewrite dtbo makefile & append_certs script
    Reflect the upstream logic rewrite in dtbo generation as needed.
    Also clean and reorganize script a bit
    
    Reference:
    [0]: https://github.com/LineageOS/android_vendor_lineage/commit/fe00ea90c9c2cd59b86b301de7bfdac518b21945
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I77890e866253e19a386785b1a3a88619bd7836db

--- configs/dtbo/append_certs.py
+++ configs/dtbo/append_certs.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 
-import sys
 import argparse
-import os
-from shutil import copyfile
+from os import stat
 
 
 def padding_file(input_file, align_num):
-    """
-    Fill 0 to make input_file's size a multiple of align_num.
-    """
-    filesize = os.stat(input_file).st_size
+    """Fill 0 to make input_file's size a multiple of align_num."""
+    filesize = stat(input_file).st_size
     file1 = open(input_file, 'ab+')
     padding = filesize % align_num
     if padding != 0:
@@ -19,28 +15,28 @@ def padding_file(input_file, align_num):
             file1.write("\x00")
     file1.close()
 
-def append_file(img_file, cert_file):
-    """
-    Append provided cert_file to the end of target img_file.
-    """
-    padding_file(img_file, 16)
+
+def append_file(img_file, cert_file, alignment):
+    """Append provided cert_file to the end of target img_file."""
+    padding_file(img_file, alignment)
     file1 = open(img_file, 'ab+')
     file2 = open(cert_file, 'rb')
     file1.write(file2.read())
     file1.close()
     file2.close()
 
+
 def main():
     parser = argparse.ArgumentParser(description='Sign an image with provided der certs')
-    parser.add_argument('--image', type=str, help='Target image')
+    parser.add_argument('--alignment', type=int, help='Alignment for calculating padding')
     parser.add_argument('--cert1', type=str, help='Certificate - cert1.der')
     parser.add_argument('--cert2', type=str, help='Certificate - cert2.der')
-    parser.add_argument('--output', type=str, help='Output image')
+    parser.add_argument('--dtbo', type=str, help='Path to the DTBO img')
     args = parser.parse_args()
 
-    copyfile(args.image, args.output)
-    append_file(args.output, args.cert1)
-    append_file(args.output, args.cert2)
+    append_file(args.dtbo, args.cert1, args.alignment)
+    append_file(args.dtbo, args.cert2, args.alignment)
+
 
 if __name__ == '__main__':
     main()
