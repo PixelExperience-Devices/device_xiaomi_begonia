f2c79b28e0dfd3d2f2fddecfda0faee2c317898a
Zinadin Zidan <zidan44@pixelexperience.org>
2020-10-12 15:15:07 +0600

    begonia: Use Prebuild APNS Config
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ie5eb942ad4f7c61c205d89ff8a5e61fd95ab6bdd

--- device.mk
+++ device.mk
@@ -45,6 +45,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/apns/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     libldacBT_dec \
