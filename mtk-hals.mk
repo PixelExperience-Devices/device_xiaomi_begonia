8b2120caeaed700e7bfde64af22656e166bbd388
TheMalachite <eliasgheeraert@gmail.com>
2020-10-17 20:46:50 +0200

    begonia: Build MTK Power HAL From OSS
    * Drop prebuild mtk power hals as we can build them from source.
    
    Change-Id: Iafaea2cb191720969d0342b6396ea169c41dd295

--- mtk-hals.mk
+++ mtk-hals.mk
@@ -13,6 +13,29 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    fstb.cfg \
+    libpowerhal \
+    power.mt6785 \
+    powerscntbl.xml \
+    libpowerhalwrap \
+    powercontable.xml \
+    libmtkperf_client \
+    libpowerhalwrap_vendor \
+    power_native_test_v_1_0 \
+    power_legacy_test_v_1_0 \
+    libmtkperf_client_vendor \
+    vendor.mediatek.hardware.power@1.1 \
+    vendor.mediatek.hardware.power@2.0 \
+    vendor.mediatek.hardware.power@2.1 \
+    vendor.mediatek.hardware.netdagent@1.0 \
+    vendor.mediatek.hardware.power@2.1-impl \
+    android.hardware.power@1.3-impl-mediatek \
+    vendor.mediatek.hardware.mtkpower@1.0 \
+    vendor.mediatek.hardware.mtkpower@1.0-impl \
+    vendor.mediatek.hardware.mtkpower@1.0-service
+
 # Telephony Jars
 PRODUCT_BOOT_JARS += \
     mediatek-common \
