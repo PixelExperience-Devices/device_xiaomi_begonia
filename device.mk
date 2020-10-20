08922f5061807754cbe6dbf9d413683583711f96
Zinadin Zidan <zidan44@pixelexperience.org>
2020-10-20 16:59:47 +0600

    begonia: Drop Prebuild Build Keystore WiFi Blobs
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: If2be0f050fa9fe51327f1868638ba358fada0b89

--- device.mk
+++ device.mk
@@ -275,6 +275,10 @@ PRODUCT_PACKAGES += \
     PixelLiveWallpaperPrebuilt
 
 # WiFi
+PRODUCT_PACKAGES += \
+    libkeystore-wifi-hidl \
+    libkeystore-engine-wifi-hidl
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
