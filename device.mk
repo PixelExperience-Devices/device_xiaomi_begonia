17cc1c991a06628736ca20f49ea23a7f55c1687f
Zinadin Zidan <zidan44@pixelexperience.org>
2020-11-03 22:25:32 +0600

    begonia: Drop Xiaomi Parts As Of Now
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I0305fb897bd75de476b23053ad8d45b08a1f58e7

--- device.mk
+++ device.mk
@@ -226,10 +226,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-# Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Telephony Jars
 PRODUCT_BOOT_JARS += \
     mediatek-common \
