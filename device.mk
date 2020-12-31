1e19bbb92dd33ed33de2e89adadbe735bb8e35a8
Kshitij Gupta <kshitijgm@gmail.com>
2020-12-31 22:32:02 +0100

    begonia: Build Audio Common 6.0 Utils
    - Fixes audio crash during boot
    
    Change-Id: Ibf6efde38ecd12938f415591d65e7e2f1e0c306e

--- device.mk
+++ device.mk
@@ -77,6 +77,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.common@6.0-util \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
