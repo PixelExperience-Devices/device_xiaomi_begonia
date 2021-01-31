0700e0662112a764f09a193a9a7d66287e0a697d
Zinadin Zidan <zidan44@pixelexperience.org>
2021-01-31 10:59:07 +0600

    begonia: Initial Skeleton Tree
    * Based on generic aosp device tree with edits for PE
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I152c42223b76a936419c82ef76e615712909d884

---
+++ aosp_begonia.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2020 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Set Shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from begonia device makefile
+$(call inherit-product, device/xiaomi/begonia/device.mk)
+
+# Inherit some PE source stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := begonia
+PRODUCT_NAME := aosp_begonia
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 8 Pro
+PRODUCT_MANUFACTURER := Xiaomi
