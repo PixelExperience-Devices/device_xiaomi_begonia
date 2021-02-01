82dfcb29dd5fcd4efe0ef1101078e8b7df3735a7
Zinadin Zidan <zidan44@pixelexperience.org>
2021-02-01 22:40:19 +0600

    begonia: Uprev DRM Blobs From Begonia A11 Release
    * Also drop old drm entry from manifest.
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Iafab8ca3815fa831d14eb1af388a30f52b00de56

--- device.mk
+++ device.mk
@@ -125,10 +125,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey \
-    android.hardware.drm@1.2-service.widevine
+    android.hardware.drm@1.0-impl:64 \
+    android.hardware.drm@1.0-service-lazy \
+    android.hardware.drm@1.3-service.clearkey
 
 # Health
 PRODUCT_PACKAGES += \
