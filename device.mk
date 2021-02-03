3a241fac11862ab5c255df8d739e72c3da362ccc
P.Adarsh Reddy <padarshr@codeaurora.org>
2021-02-03 19:48:16 +0600

    begonia: Up-rev Health hal from 2.0 to 2.1
    Change-Id: Id091dc7b7116d93046f3ce36c93ec73fb1f4ca21

--- device.mk
+++ device.mk
@@ -136,8 +136,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
