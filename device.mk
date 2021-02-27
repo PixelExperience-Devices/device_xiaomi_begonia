06394d58808ddaa3b9d3bc8ba63907acf8e0ef68
Zinadin Zidan <zidan44@pixelexperience.org>
2021-02-27 09:30:14 +0600

    begonia: nfc: Drop nfc Secure Element hals
    * Its constantly crashing in logs.
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I243eedb2e925720766ef19d4dd4b8e1773a8ceb9

--- device.mk
+++ device.mk
@@ -197,10 +197,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Tag \
     NfcNci \
-    SecureElement \
     com.android.nfc_extras \
-    android.hardware.nfc@1.1-service \
-    android.hardware.secure_element@1.0-service
+    android.hardware.nfc@1.1-service
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
