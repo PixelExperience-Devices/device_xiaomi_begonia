8dade48376f4983a810c50058399f30d2e33788a
Zinadin Zidan <zidan44@pixelexperience.org>
2021-07-10 17:07:22 +0600

    begonia: media: Build media Codec2 modules
    * https://source.android.com/devices/media/updatable-media
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ied9574bce55d1c27ef48810dfd07a9e2929be912

--- device.mk
+++ device.mk
@@ -190,6 +190,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Media Codec2 modules
+PRODUCT_PACKAGES += \
+    com.android.media.swcodec \
+    libsfplugin_ccodec
+
 # Minijail
 PRODUCT_PACKAGES += \
     libavservices_minijail \
