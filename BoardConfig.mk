e9bb6aa92ec38045f0a58f5141458724bafc4e60
Zinadin Zidan <zidan44@pixelexperience.org>
2021-07-10 21:11:23 +0600

    begonia: Update blobs from begonia V12.5.1.0.RGGMIXM
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I8262c9b5032227f6c47e027153812b43ece7f72c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,7 +111,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-06-01
+VENDOR_SECURITY_PATCH := 2021-07-01
 
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
