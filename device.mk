8351c7d4421da8b77f74659a6a3cefb0479bf4c8
SamarV-121 <samarvispute121@gmail.com>
2021-08-04 18:44:57 +0600

    begonia: recovery: Build init.recovery.mt6785.rc
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I98f78afb0cbd7dac50574cdd8f01c6a751c7e4fb

--- device.mk
+++ device.mk
@@ -240,6 +240,10 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
+# Recovery
+PRODUCT_PACKAGES += \
+    init.recovery.mt6785.rc
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
---
+++ recovery/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.recovery.mt6785.rc
+LOCAL_SRC_FILES := root/$(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_OUT)/root
+include $(BUILD_PREBUILT)
