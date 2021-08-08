ef76530adeced2caea46d3d8a7ca2bf0346462fe
Jarl-Penguin <jarlpenguin@outlook.com>
2021-08-08 10:38:48 +0300

    begonia: Add odm/product/system_ext partitions
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I7e619eb12cd9c2f333336369dd66cb22b2f8dd03

--- releasetools.py
+++ releasetools.py
@@ -16,6 +16,42 @@
 
 import common
 import re
+import os
+from common import BlockDifference, EmptyImage, GetUserImage
+
+# The joined list of user image partitions of source and target builds.
+# - Items should be added to the list if new dynamic partitions are added.
+# - Items should not be removed from the list even if dynamic partitions are
+#   deleted. When generating an incremental OTA package, this script needs to
+#   know that an image is present in source build but not in target build.
+USERIMAGE_PARTITIONS = [
+    "odm",
+    "product",
+    "system_ext",
+]
+
+def GetUserImages(input_tmp, input_zip):
+  return {partition: GetUserImage(partition, input_tmp, input_zip)
+          for partition in USERIMAGE_PARTITIONS
+          if os.path.exists(os.path.join(input_tmp,
+                                         "IMAGES", partition + ".img"))}
+
+def FullOTA_GetBlockDifferences(info):
+  images = GetUserImages(info.input_tmp, info.input_zip)
+  return [BlockDifference(partition, image)
+          for partition, image in images.items()]
+
+def IncrementalOTA_GetBlockDifferences(info):
+  source_images = GetUserImages(info.source_tmp, info.source_zip)
+  target_images = GetUserImages(info.target_tmp, info.target_zip)
+
+  # Use EmptyImage() as a placeholder for partitions that will be deleted.
+  for partition in source_images:
+    target_images.setdefault(partition, EmptyImage())
+
+  # Use source_images.get() because new partitions are not in source_images.
+  return [BlockDifference(partition, target_image, source_images.get(partition))
+          for partition, target_image in target_images.items()]
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
