36c9cae8a4e2370d86f9cddc988bf6fcb8ea826b
TheMalachite <eliasgheeraert@gmail.com>
2021-09-09 23:52:23 +0600

    begonia: Get back to AOSP vibrator HAL
    * MediaTek vibrator HALs weirdly behave on AOSP (Weak strenght, randomly work)
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I0ef75f9418b70eb63524b9e4c0fd639de8740b0c

--- device.mk
+++ device.mk
@@ -279,6 +279,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+     android.hardware.vibrator@1.0-impl \
+     android.hardware.vibrator@1.0-service
+
 # WiFi
 PRODUCT_PACKAGES += \
     hostapd \
