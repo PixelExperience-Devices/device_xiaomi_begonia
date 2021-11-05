972e08b18ba75c63215a2659dd431dade03ba480
Zinadin Zidan <zidan.roking@gmail.com>
2021-11-05 18:44:39 +0600

    begonia: sepolicy: Update SEPolicy definitions
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: Id9a6f160743dc3b5c3e584aa0342f4117a7a0479

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,8 +153,8 @@ VENDOR_SECURITY_PATCH := 2021-07-01
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # VNDK
