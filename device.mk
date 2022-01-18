f8cdef7eaa219923ee21b04fe31c03aa6f524890
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-18 19:53:39 +0600

    begonia: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I42c3cea0bb0e63f87c37c73f5c9f736048f71755

--- device.mk
+++ device.mk
@@ -84,7 +84,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.3-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
