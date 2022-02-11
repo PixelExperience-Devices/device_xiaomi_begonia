5df0b0c425893bf63348e560deeeb469424962c0
Zinadin Zidan <zidan44@pixelexperience.org>
2022-02-11 18:20:29 +0600

    begonia: prop: Move props to vendor props
    * Also reorder props.
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I2dbd271e69d9a15ec219c2806b40f6cc7f727f92

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,7 +145,6 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
