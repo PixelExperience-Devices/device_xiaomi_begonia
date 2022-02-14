9559a9ec2c01b1bc9a1a30ca4d4e9b7903baf01d
Zinadin Zidan <zidan44@pixelexperience.org>
2022-02-14 21:55:01 +0600

    begonia: Use pe recovery as default recovery
    This reverts commit 479686efb3e4b0c63d8735c2f34fa5ff76b16b1a.
    
    Change-Id: Ia41cd5f05c1e2de5875111fdcaabc93ac9556a74

--- aosp_begonia.mk
+++ aosp_begonia.mk
@@ -26,6 +26,7 @@ $(call inherit-product, device/xiaomi/begonia/mtk-interfaces.mk)
 # Inherit some PE source stuff
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_AOSP_RECOVERY := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Boot Animation
