e46b94d06d32b7fddf627d4a297b43da5a00c703
Zinadin Zidan <zidan44@pixelexperience.org>
2022-02-16 08:18:59 +0600

    begonia: Update blobs from miui RP1A.200720.011
    * From: begonia-user 11 RP1A.200720.011 V12.5.7.0.RGGMIXM release-keys
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ic46ab347ca48163f3e3516b80e75f839aa8c9b29

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,7 +144,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-12-01
+VENDOR_SECURITY_PATCH := 2022-02-01
 
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
