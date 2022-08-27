1bf70778032180b4bc5e62351824efc63d276e4c
telepathine <36798666+telepathine@users.noreply.github.com>
2022-08-27 16:43:16 +0100

    begonia: InCallService: Map AOSP gain step range to MTK one
    MTK has 15 volume steps, we have whatever is set in vc_call_vol_steps.
    Mapping our range to theirs allows reaching higher volume levels.
    
    A log curve is used to make gain transitions less abrupt at higher volumes.
    
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>
    Change-Id: Ic503f18cd126ff75e1b70b35bddf1e971a110ef9

--- app/InCallService/src/org/lineageos/mediatek/incallservice/GainUtils.java
+++ app/InCallService/src/org/lineageos/mediatek/incallservice/GainUtils.java
@@ -1,5 +1,6 @@
 package org.lineageos.mediatek.incallservice;
 
+import android.os.SystemProperties;
 import android.media.AudioSystem;
 
 import android.util.Log;
@@ -8,8 +9,13 @@ public class GainUtils {
     public static final String LOG_TAG = "MtkInCallService";
 
     public static void setGainLevel(int audioDevice, int gainIndex, int streamType) {
+        int maxStep = SystemProperties.getInt("ro.config.vc_call_vol_steps", 7);
         String parameters = String.format("volumeDevice=%d;volumeIndex=%d;volumeStreamType=%d",
-                                          audioDevice, Math.min(7, gainIndex), streamType);
+                                          audioDevice,
+                                          Math.round(
+                                            (15.0 / Math.log(maxStep + 1.0))
+                                            * Math.log(Math.min(maxStep, gainIndex) + 1.0)),
+                                          streamType);
         Log.d(LOG_TAG, "Setting audio parameters to: " + parameters);
         AudioSystem.setParameters(parameters);
     }
