df0c2386d16660885fefbf07f23a519f3c50779c
Zinadin Zidan <zidan44@pixelexperience.org>
2023-02-11 00:51:06 +0600

    begonia: Update from miui V12.5.15.0.RGGEUXM
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Iacc26077578a53092e28410341f59474885bc5cc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,7 +141,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-02-01
+VENDOR_SECURITY_PATCH := 2023-02-01
 
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
